9bda56b76ae34397073043118679e7e624e3cab8
Daniel Boll <danielboll.academico@gmail.com>
2024-12-07 10:50:53 -0300

    feat(lidar): change lidar length for different scans
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- components/camera/test/manager.gd
+++ components/camera/test/manager.gd
@@ -1,13 +1,14 @@
 extends Camera3D
 
 @export var scan_interval: float = 8.5
-@export var ray_count: int = 100
-@export var ray_length: float = 20.0
+@export var ray_count: int = 500
+@export var ray_length_scan: float = 20.0
+@export var ray_length_sweep: float = 30.0
 @export var sweep_steps_horizontal: int = 100
 @export var sweep_steps_vertical: int = 100
 @export var point_size: float = 2.5
 @export var fade_power: float = 0.15
-@export var environment_scan_cooldown: float = 0.4
+@export var environment_scan_cooldown: float = 0.8
 @export var full_sweep_scan_cooldown: float = 3.6
 
 var point_positions = []
@@ -20,6 +21,7 @@ var lidar_full_sfx = preload("res://sfx/lidar-full.mp3")
 var last_environment_scan_time: float = -1000.0
 var last_full_sweep_scan_time: float = -1000.0
 
+
 func _ready():
 	point_mesh_instance = MeshInstance3D.new()
 	get_tree().get_root().add_child.call_deferred(point_mesh_instance)
@@ -30,8 +32,9 @@ func _ready():
 	point_mesh_instance.material_override = point_material
 
 	point_material.set_shader_parameter("point_size", point_size)
-	point_material.set_shader_parameter("max_fade_distance", ray_length)
+	point_material.set_shader_parameter("max_fade_distance", ray_length_sweep)
 	point_material.set_shader_parameter("fade_power", fade_power)
+
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -39,6 +42,7 @@ func _input(event):
 			perform_environment_scan()
 		elif event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			perform_full_sweep()
+
 
 func _process(delta):
 	var points_updated = false
@@ -50,11 +54,13 @@ func _process(delta):
 	if points_updated:
 		point_positions = point_positions.filter(filter_by_time_left)
 		update_point_mesh()
-	
+
 	point_material.set_shader_parameter("player_position", global_position)
+
 
 func filter_by_time_left(p):
 	return p["time_left"] > 0
+
 
 func perform_environment_scan():
 	var current_time = Time.get_ticks_msec() / 1000.0
@@ -64,16 +70,16 @@ func perform_environment_scan():
 	last_environment_scan_time = current_time
 	$"../AudioStreamPlayer".stream = lidar_sfx
 	$"../AudioStreamPlayer".play()
-	
+
 	var camera_forward = -global_transform.basis.z
 
 	for i in range(ray_count):
-		var ray_direction = camera_forward.rotated(Vector3.UP, randf_range(-PI/4, PI/4))
-		ray_direction = ray_direction.rotated(Vector3.RIGHT, randf_range(-PI/4, PI/4))
+		var ray_direction = camera_forward.rotated(Vector3.UP, randf_range(-PI / 4, PI / 4))
+		ray_direction = ray_direction.rotated(Vector3.RIGHT, randf_range(-PI / 4, PI / 4))
 		ray_direction = ray_direction.normalized()
 
 		var ray_origin = global_position
-		var ray_target = ray_origin + ray_direction * ray_length
+		var ray_target = ray_origin + ray_direction * ray_length_scan
 
 		var space_state = get_world_3d().direct_space_state
 		var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_target)
@@ -82,16 +88,19 @@ func perform_environment_scan():
 
 		if result:
 			var collider = result.collider
-			if (!is_node_in_group_or_parent(collider, "monster")):
+			if !is_node_in_group_or_parent(collider, "monster"):
 				var is_red = is_node_in_group_or_parent(collider, "red")
 				var is_green = is_node_in_group_or_parent(collider, "green")
-				point_positions.append({
-					"position": result.position,
-					"is_red": is_red,
-					"is_green": is_green,
-					"time_left": scan_interval
-				})
+				point_positions.append(
+					{
+						"position": result.position,
+						"is_red": is_red,
+						"is_green": is_green,
+						"time_left": scan_interval
+					}
+				)
 	update_point_mesh()
+
 
 func update_point_mesh():
 	var vertices = PackedVector3Array()
@@ -99,14 +108,14 @@ func update_point_mesh():
 
 	for point in point_positions:
 		vertices.append(point["position"])
-		var color : Color
-		
+		var color: Color
+
 		if point["is_red"]:
 			color = Color(1, 0, 0, 1)
 		elif point["is_green"]:
-			color = Color(0, 1, 0, 1) 
+			color = Color(0, 1, 0, 1)
 		else:
-			color = Color(1, 1, 1, 1) 
+			color = Color(1, 1, 1, 1)
 		colors.append(color)
 
 	var arrays = []
@@ -119,6 +128,7 @@ func update_point_mesh():
 		mesh.add_surface_from_arrays(Mesh.PRIMITIVE_POINTS, arrays)
 		point_mesh_instance.mesh = mesh
 
+
 func is_node_in_group_or_parent(node: Node, group_name: String) -> bool:
 	var current_node = node
 	while current_node != null:
@@ -126,6 +136,7 @@ func is_node_in_group_or_parent(node: Node, group_name: String) -> bool:
 			return true
 		current_node = current_node.get_parent()
 	return false
+
 
 func perform_full_sweep():
 	var current_time = Time.get_ticks_msec() / 1000.0
@@ -135,7 +146,7 @@ func perform_full_sweep():
 	last_full_sweep_scan_time = current_time
 	$"../AudioStreamPlayer".stream = lidar_full_sfx
 	$"../AudioStreamPlayer".play()
-	
+
 	var viewport_size = get_viewport().get_texture().get_size()
 	var space_state = get_world_3d().direct_space_state
 	var query = PhysicsRayQueryParameters3D.new()
@@ -164,19 +175,22 @@ func perform_full_sweep():
 				var ray_direction = project_ray_normal(screen_pos)
 
 				query.from = ray_origin
-				query.to = ray_origin + ray_direction * ray_length
+				query.to = ray_origin + ray_direction * ray_length_sweep
 
 				var result = space_state.intersect_ray(query)
 				if result:
 					var collider = result.collider
-					if (!is_node_in_group_or_parent(collider, "monster")):
+					if !is_node_in_group_or_parent(collider, "monster"):
 						var is_red = is_node_in_group_or_parent(collider, "red")
 						var is_green = is_node_in_group_or_parent(collider, "green")
-						point_positions.append({
-							"position": result.position,
-							"is_red": is_red,
-							"is_green": is_green,
-							"time_left": scan_interval
-						})
+						point_positions.append(
+							{
+								"position": result.position,
+								"is_red": is_red,
+								"is_green": is_green,
+								"time_left": scan_interval
+							}
+						)
+			await get_tree().process_frame
 		update_point_mesh()
-		await get_tree().process_frame
+	$"../AudioStreamPlayer".stop()
