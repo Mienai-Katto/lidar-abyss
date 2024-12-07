0be870b2c97910b3a1181f3a34bfc3af13f9a81c
Flávio Zanoni <flaviozg888@gmail.com>
2024-12-06 23:32:51 -0300

    Feat/sfx (#2)

--- levels/lobby/manager.gd
+++ levels/lobby/manager.gd
@@ -6,33 +6,38 @@ var current_player_name: String
 var players_in_room: Array
 var connected_players: Array = []
 
+var lobby_bgm = preload("res://sfx/lobby_bgm.mp3");
+
 func _ready():
-  # var room_data = Lobby.room_data
-  # room_name = room_data["room"]["name"]
-  # room_id = room_data["room"]["id"]
-  # current_player_name = room_data["player"]["name"]
-  # players_in_room = room_data["room"]["players"]
+	# var room_data = Lobby.room_data
+	# room_name = room_data["room"]["name"]
+	# room_id = room_data["room"]["id"]
+	# current_player_name = room_data["player"]["name"]
+	# players_in_room = room_data["room"]["players"]
 
-  update_lobby_ui()
-
-  multiplayer.peer_connected.connect(on_peer_connected)
-  multiplayer.peer_disconnected.connect(on_peer_disconnected)
+	update_lobby_ui()
+	$AudioStreamPlayer.bus = "menu_bus"
+	if (!$AudioStreamPlayer.playing):
+		$AudioStreamPlayer.stream = lobby_bgm
+		$AudioStreamPlayer.play()
+	multiplayer.peer_connected.connect(on_peer_connected)
+	multiplayer.peer_disconnected.connect(on_peer_disconnected)
 
 func on_peer_connected(peer_id):
-  connected_players.append(peer_id)
-  update_lobby_ui()
+	connected_players.append(peer_id)
+	update_lobby_ui()
 
 func on_peer_disconnected(peer_id):
-  connected_players.erase(peer_id)
-  update_lobby_ui()
+	connected_players.erase(peer_id)
+	update_lobby_ui()
 
 func update_lobby_ui():
-  %RoomInfo.text = "Room: %s (%s)" % [room_name, room_id]
-  %PlayerList.text = "Players in Room:\n" + "\n".join(players_in_room)
+	%RoomInfo.text = "Room: %s (%s)" % [room_name, room_id]
+	%PlayerList.text = "Players in Room:\n" + "\n".join(players_in_room)
 
-  # Highlight connected players
-  var connected_names = []
-  for player in players_in_room:
-    if player["id"] in connected_players:
-      connected_names.append(player["name"])
-      %ConnectedPlayers.text = "Connected:\n" + "\n".join(connected_names)
+	# Highlight connected players
+	var connected_names = []
+	for player in players_in_room:
+		if player["id"] in connected_players:
+			connected_names.append(player["name"])
+			%ConnectedPlayers.text = "Connected:\n" + "\n".join(connected_names)
--- levels/menu/manager.gd
+++ levels/menu/manager.gd
@@ -5,30 +5,35 @@ extends Node
 
 @export var http: AwaitableHTTPRequest
 
+var lobby_bgm = preload("res://sfx/lobby_bgm.mp3");
+
 func _ready():
-  enter_button.disabled = true
+	enter_button.disabled = true
+	$AudioStreamPlayer.bus = "menu_bus"
+	$AudioStreamPlayer.stream = lobby_bgm
+	$AudioStreamPlayer.play()
 
 func _process(_delta: float) -> void:
-  enter_button.disabled = code_input.text.length() != 12
+	enter_button.disabled = code_input.text.length() != 12
 
 func _on_enter_button_pressed():
-  var code = code_input.text
-  get_player_info(code)
+	var code = code_input.text
+	get_player_info(code)
 
 func get_player_info(code: String) -> void:
-  var url = "http://localhost:3000/api/game/" + code
-  var headers = ["Content-Type: application/json"]
-  var resp = await http.async_request(url, headers)
-  if resp.success() and not resp.status_err():
-    var player_data = resp.body_as_json()
-    if player_data:
-      connect_to_game_server(player_data)
-    else:
-      print("Failed to parse player data.")
-  else:
-    print("Failed to get data.")
+	var url = "http://localhost:3000/api/game/" + code
+	var headers = ["Content-Type: application/json"]
+	var resp = await http.async_request(url, headers)
+	if resp.success() and not resp.status_err():
+		var player_data = resp.body_as_json()
+		if player_data:
+			connect_to_game_server(player_data)
+		else:
+			print("Failed to parse player data.")
+	else:
+		print("Failed to get data.")
 
 func connect_to_game_server(room_data: Dictionary) -> void:
-  await Lobby.join_game("127.0.0.1", room_data["room"], room_data["player"])
-  Lobby.load_game("res://levels/map-01/map-01.tscn")
-  # transition_to_lobby_ui(room_data)
+	await Lobby.join_game("127.0.0.1", room_data["room"], room_data["player"])
+	Lobby.load_game("res://levels/map-01/map-01.tscn")
+	# transition_to_lobby_ui(room_data)
